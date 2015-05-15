e1a39c166cbc15def0c71bf9505f83933b04c047
James <lalalili@gmail.com>
2015-05-15 18:24:12 +0800

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,6 +26,8 @@
     <!-- Custom Fonts -->
     <link href="{{ asset('/font/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
